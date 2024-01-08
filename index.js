2341a7d284e82cbb93d56e20a2a3a78891c77a02
cschwatz <criistianrs@hotmail.com>
2024-01-08 14:18:01 -0300

    exercise 3.4

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id) // request parameters returns a string type
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
