cfb3fef897163c80e9873d70ced19207983b3f20
cschwatz <criistianrs@hotmail.com>
2024-01-11 17:42:36 -0300

    exercise 3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        validate: {
+            validator: function(v) {
+              return /\d{2,3}-\d+/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`,
+        },
+        required: [true, 'User phone number is required']
     }
 })
 
